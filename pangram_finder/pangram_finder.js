ae5653b40448f7be4580caf85f5e8ea6fc112a94
Chris Barclay <chris.barclay1@gmail.com>
2023-03-01 18:39:03 +0000

    Completed panagram - messy

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,17 @@
 const PangramFinder = function (phrase) {
+  this.phrase = phrase;
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  const splitPhrase = this.phrase.split('');
+  return answer = this.alphabet.every(letter => {    
+    for (let char of this.phrase){
+      if (char.toLowerCase() === letter){
+        return true;
+      }
+    }
+  })
 }
 
 module.exports = PangramFinder;
